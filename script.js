b852b3643f60a1b704fa224ef2374bd33723ba9b
Vanessa Joy Regalado <regaladovanessa441@gmail.com>
2021-01-17 21:32:22 +0800

    rechange

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
-function Firstbutton() {
-    if (document.getElementById("secondview").style.display === 'none', document.getElementById("firstview").style.display === 'block') {
-        document.getElementById("secondview").style.display = 'block';
-        document.getElementById("firstview").style.display = 'none';
-    } else {
-        document.getElementById("secondview").style.display = 'none';
-        document.getElementById("firstview").style.display = 'block';
-    }
+function randomize() {
+    document.getElementById('image').style.backgroundColor = randomColors();
 }
 
-function ramdom() {
-    var x = "0123456789ABCDRF";
-    var y = "#";
-    for (i = 0; i < 6; i++) {
-        y = y + x[Math.floor(Math.random() * 16)];
-    }
-    document.getElementById("image").style.backgroundColor = y;
+function randomColors() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
 
-function secondbutton() {
-    var btnS = document.getElementById("ramdomID");
-    var color = btnS.options[btnS.selectedIndex].value;
+function showcolor() {
+    var color = document.getElementById("colorlist").value
     document.getElementById("image").style.backgroundColor = color;
 }
 
-function thirdbutton() {
-    document.getElementById("image").style.backgroundColor = 'white';
+function reset() {
+    document.getElementById("image").style.backgroundColor = "white";
 }
